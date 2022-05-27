8471d7f5931ae6b279d077f17838661bf64d65f1
nainasinha93 <naina1993sinha@gmail.com>
2022-05-27 13:36:40 +0530

    popup

--- src/Component/Food.jsx
+++ src/Component/Food.jsx
@@ -3,38 +3,37 @@ import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 import img from "../Image/images.png"
 import '../Component/Food.css'
+import Showpop from "./Showpop";
 
 function Food(props){
   //  let imgs=[img,img,img,img];
     const [mouseEvenet,setMouseEvent]=useState(false)
-    const [showPopup,setShowPopup]=useState(false)
+    const [showPopup,setShowPopup]=useState("")
 
 useEffect(()=>{
-   // console.log(props.s.meals[0].strMealThumb)
+  // console.log(props.s.meals[0].strMealThumb)
 })
 
 const showName=(i,a)=>{
  let hover=document.getElementById(i)
  hover.classList.add("hover")
- setShowPopup(true)
+ setShowPopup(i)
  //popup
  let getImage=document.getElementById(i+1);
     getImage.classList.add("imagesrc")
-    console.log(getImage)
-
- //console.log(hover)
+  
 }
 const removeName=(i,a)=>{
-  //  console.log(i)
+  
     let hover=document.getElementById(i)
     let elem=hover.innerHTML;
-   // console.log(elem)
+   
     hover.classList.remove("hover")
-    setShowPopup(false)
+    setShowPopup("")
     // popup
     let getImage=document.getElementById(i+1);
     getImage.classList.remove("imagesrc")
-    console.log(getImage)
+  
 }
 
     return(
@@ -46,12 +45,18 @@ const removeName=(i,a)=>{
         return(<>
       
         <div key={i} id={a.idMeal} className="col item-col" onMouseEnter={(i)=>{showName(a.idMeal,a)}} onMouseLeave={(i)=>{removeName(a.idMeal,a)}} >
-           {/*<Popup  className="position" trigger={<button> Trigger</button>} position="right center"><div>Popup content here !!</div></Popup> */}
-      
-           {showPopup?<div id={a.idMeal+1}  className="position"  style={{color:"black"}}> Hi Add pop up here</div>:null}
+           {/*<Popup  className="position" trigger={<button> Trigger</button>} position="right center"><div>
+            <img className="" src={a.strMealThumb} style={{marginBottom:"8px", height:"200px",width:"200px"}}></img>
+           </div></Popup> */}
+        
+        {showPopup===a.idMeal?<div id={a.idMeal+1}  className="position"  style={{color:"black"}}>{a.strMeal}
+        
+         </div>:null}
+        
         <img className="" src={a.strMealThumb} style={{marginBottom:"8px", height:"200px",width:"200px"}}></img>
         </div>
-     
+
+   
         </>)
     })}
 
